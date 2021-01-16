98e63fd40fee97f4c060eafb7208371f417b8be4
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-01-16 18:57:25 -0500

    created the post model and api routes for post model

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -11,7 +11,6 @@ router.get('/', (req, res) => {
       'post_content',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     order: [['created_at', 'DESC']],
     include: [
@@ -36,46 +35,33 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
-
+//to get a single post 
 router.get('/:id', (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'post_content',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'post_content', 'title', 'created_at'],
+      include: [
+        {
           model: User,
           attributes: ['username']
         }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-      res.json(dbPostData);
+      ]
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
 
 // api/posts/
 router.post("/", (req, res) => {
--- models/index.js
+++ models/index.js
@@ -3,6 +3,12 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 //associations
+
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
 Post.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete:"CASCADE"
