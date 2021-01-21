2faebd427a46b4c6843bd114d059461311797bd9
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-01-21 18:14:28 -0500

    added helpers

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
 //may or may not need this?
-const routes = require('./controllers');
+//const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,7 +25,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,15 +1,16 @@
 
 module.exports = {
-    format_date: (date) => {
-      return `${new Date(date).getMonth() + 1}/${new Date(
+    format_date: date => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
         date
-      ).getDate()}/${new Date(date).getFullYear()}`;
+      ).getFullYear()}`;
     },
     format_plural: (word, amount) => {
-      if (amount !== 1) {
-        return `${word}s`;
+        if (amount !== 1) {
+          return `${word}s`;
+        }
+    
+        return word;
       }
-      return word;
-    },
-  };
+    }
   
